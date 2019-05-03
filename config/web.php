1932de6a424c50690d43a87ab0b5b671821eb01d
maxkamov <maxkamov48@gmail.com>
2019-05-03 22:12:16 +0300

    cookie validation

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'dsfgzhfjgmgzdbfzdvcxzvszvds',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
